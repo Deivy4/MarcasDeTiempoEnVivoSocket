eaf16d12d9be7c8fcc05f57d506fbe22889ffa37
David Ascanio <daviddeveloper24@outlook.com>
2025-07-26 20:11:27 -0500

    se agrega cors

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ wss.on("connection", (ws) => {
 console.log("WebSocket server is running on ws://localhost:8080");
 
 import express from "express";
-
+import cors from "cors";
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.post("/setTime", (req, res) => {
   const { newTime } = req.body;
   if (newTime) {
